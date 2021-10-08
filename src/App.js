3ea402e9fb3f0e8e46a43c00382fb6ac77b04ee8
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2021-10-09 00:45:17 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import './App.css'
+import BookShelf from '../../my-reads-app/src/components/BookShelf'
+import BookSearch from "../../my-reads-app/src/components/BookSearch";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BooksApp extends React.Component {
+  state = {
+    mybooks: [],
+    searchedBooks: []
+  }
+
+  handleAddBook = (book) => {
+    this.setState(prevState => {
+      const filteredBooks = prevState.mybooks.filter(b => b.id !== book.id)
+      return {
+        mybooks: [...filteredBooks, book]
+      }
+    }, () => {
+      BooksAPI.update(book, book.shelf).then(b => {
+
+      }).catch(e => {
+
+      })
+    })
+  }
+
+  handleSearch = (term) => {
+    if(term.length > 0){
+      BooksAPI.search(term)
+      .then((books) => {
+        if(books && !books.error){
+          this.setState({
+              searchedBooks: books.map(book => {
+                  const currentLibrary = this.state.mybooks.find(b => b.id === book.id)
+                  if(currentLibrary)
+                      book.shelf =  currentLibrary.shelf
+                  return book
+              })
+          })
+      }
+        else {
+          this.setState({
+            searchedBooks: []
+          })
+        }
+      }).catch(e => {
+        this.setState({
+          searchedBooks: []
+        })
+      })
+    }
+    else{
+      this.setState({
+        searchedBooks: []
+      })
+    }
+  }
+
+  handleResetSearch = () => {
+    this.setState({
+        searchedBooks: []
+    })
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => (this.setState({
+      mybooks: books
+    })))
+  }
+
+  render() {
+    const { mybooks } = this.state
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <BookShelf books={mybooks} addBook={this.handleAddBook} />
+        )} />
+        <Route path="/search" render={() => (
+          <BookSearch 
+            addBook={this.handleAddBook} 
+            currentLibary={mybooks} 
+            search={this.handleSearch} 
+            searchedBooks={this.state.searchedBooks}
+            resetSearch={this.handleResetSearch}
+          />
+        )} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default BooksApp
